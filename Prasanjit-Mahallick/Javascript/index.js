b37e15c77642889cf015f549b82b22ead11ab565
prasanjit0611 <mahallickprasanjit@gmail.com>
2026-02-20 18:30:55 +0530

    1st JS class

---
+++ Prasanjit-Mahallick/Javascript/index.js
@@ -0,0 +1,43 @@
+// console.log(5**4)
+// console.log(true)
+// console.log(4.95-2.55)
+
+// var x = 18
+// let y = 7 
+// const z = 45
+// var vk = "Virat Kholi"
+
+// x =10
+// y = 12
+// z = 11
+
+// var x//declaration
+// x=10//initialisation
+// x=11 //re-assignment
+
+// const x = 10 ; it is necessary to initialize a 
+
+// var is global scoped and functional scope
+// {
+//     let y=25
+//     var x=10
+//     console.log(x)
+//     console.log(y)
+//     console.log(x+y)
+//     let u;
+// }
+// console.log(x)
+// console.log(y)
+
+// console.log(x)
+// var x = 12;
+
+{
+    let x = 10;
+    console.log(x)
+    {
+        console.log(x)
+        let x = 12
+        console.log(x)
+    }
+}
