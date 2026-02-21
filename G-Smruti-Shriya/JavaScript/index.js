8bb21f870fc9ec7c1f2f8e06460e6c8673e006e5
G Smruti Shriya <gsmrutishriya12@gmail.com>
2026-02-21 17:54:23 +0530

    JavaScript1stclass

---
+++ G-Smruti-Shriya/JavaScript/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+//Delaring Variables 
+//var
+var name = "G SMruti Shriya"
+name = "Smruti"
+console.log(name)
+// Doesnt respect any scope
+{
+    var age = 18 // block scope
+}
+console.log(age)
+
+//functional scope
+
+function Varname(){
+    var age = 48
+}
+console.log(age)
+
+//let
+let nme = "G Smruti Shriya"
+console.log(nme)
--- G-Smruti-Shriya/practice.py
+++
@@ -1,2 +0,0 @@
-print ("Hello World")
-print("Sum : ",4+5)
