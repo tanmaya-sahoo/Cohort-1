80c52c029acf28f16728aac3979d5197bcff6995
jyotiswadhindibya-bit <jyotiswadhindibya@gmail.com>
2026-02-21 18:15:28 +0530

    JS fundamentals

---
+++ Swadhin-Dibya-Jyoti/Javascript/script.js
@@ -0,0 +1,24 @@
+console.log("HELLO WORLD");
+console.log("Second");
+
+//variables
+//var let const
+//var is fuction scoped
+
+//properties of var are: redeclaration,reassignment,global scope,functional scope
+// when we use var and use that variable anywhere it shows a strike on it
+
+
+//let
+//It properties are no-redeclaration,reassignment
+// It scopes are global scope, functional scope and block scope
+
+//const
+//it properties are only declararion not allowed,no reassignment,it also has same scope as let
+
+
+//hoisting
+//console.log(a);
+//var a=10; or let a=10;
+
+//Js first reads the file then compile it so when it reads console.log(a) it craetes a var a so in cas of var it not shows error and in let  or const it shows error as let and const doesnt allow redeclaration
