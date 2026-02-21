f90ff1eadb70c58b8e4d450c00cf7af5235b91c1
AdityaSekharDas <adityasekhardas09@gmail.com>
2026-02-21 18:15:31 +0530

    JS Fundamentals

---
+++ Aditya-Sekhar-Das/Javascript/index.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log("Adi");
+
+const name = "Aditya";
+console.log(name);
+
+const age = 18;
+console.log(age);
+
+{
+    const age1 = 20;
+}
+
+console.log(age1);
+
+
+
+
