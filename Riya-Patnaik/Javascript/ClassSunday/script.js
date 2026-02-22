e53006f14e15e2c1e2c00f7447c9cfae4692763c
Riya Patnaik <riya.ofc7@gmail.com>
2026-02-22 13:12:54 +0530

    Class 1 notes

---
+++ Riya-Patnaik/Javascript/ClassSunday/script.js
@@ -0,0 +1,43 @@
+// console.log("hello")
+
+// var x=10
+// function name(){
+//     var y=11
+//     console.log(x)
+//     console.log(y)
+// }
+// console.log(y)
+// name()
+
+// var x;//declaration
+// x=10//initialization
+// x=11//re-assignment
+
+//var
+//globally and functional scope
+//redeclare and re-initialize
+
+//let
+//globally and block scope
+//cannot redeclare and re-initialize
+
+// let x=10
+// //inside local scope
+// {
+//     let x=41
+// }
+// function name(){
+//     var y=11
+//     console.log(x)
+//     console.log(y)
+// }
+// // console.log(y)
+// name()
+
+/*const
+global and block scope
+redeclare and reinitialize both not possible
+value caanot be changed
+*/
+
+const x=10;
