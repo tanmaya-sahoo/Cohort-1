ed8b5b4b17bcdd1c1253581ff0e7cc895b087636
ishanyadav-hash <ishanyadav2707@gmail.com>
2026-02-22 15:54:22 +0530

    Sync arrow callback

--- Ishan-Yadav/JavaScript/class sunday/script.js
+++ Ishan-Yadav/JavaScript/class sunday/script.js
@@ -29,3 +29,61 @@ value can't be changed */
 // const x=10
 // x=11 not possible
 
+// let arr=[1,2,3]
+// let obj={
+//     "Student":"Hello",
+//     "Mentor":"Pratham"
+
+// }
+
+// console.log(arr)
+// console.log(obj)
+// console.log(arr[0])
+// console.log(obj.Student)
+
+
+//  function print(a,b){
+//      console.log(a+b)
+//  }
+//  print(2,"3") // + string ko prefer karta hai
+
+// function name(a,b){
+//      console.log(a-b)
+//  }
+// name(2,"3") // - number ko prefer karta hai
+
+// Arrow function
+
+// function print(a,b){
+//     console.log(a+b)
+// }
+
+// const fn=() => {
+//     console.log("Arrow")
+// }
+
+// fn()
+
+
+// // High order function
+// function ho(a,b,cb){ // cb is acall back function
+//     console.log(a,b,cb)
+// }
+
+// ho(2,3,print)
+
+// console.log("Start")
+// setTimeout(()=>{
+//     console.log("Boiling Milk")
+// },2000)
+// console.log("end")
+
+// console.log("Start")
+
+// console.log("end")
+
+
+const arrow=(...args)=>{
+    console.log(args)
+}
+arrow("arrow1","arrow2")
