d3ae87387895f56981a6f6178298d9588448d5a5
Jayita Mandal <jayitamandal1245@gmail.com>
2026-02-22 15:54:38 +0530

    more notes

--- Jayita-Mandal/JavaScript/260222/script.js
+++ Jayita-Mandal/JavaScript/260222/script.js
@@ -16,3 +16,43 @@ function name(){
 }
 //console(y)
 name()
+
+
+let arr=[1,2,3,4]
+
+//key value pair
+/*
+    let/const obj_name={
+        "key1":"value1",
+        "key1":"value1",
+        "key1":"value1",
+        "key1":"value1"
+    }
+*/
+let obj={
+    "Student":"Himadri",
+    "Mentor":"Pratham"
+}
+console.log(arr[0])
+console.log(obj.Student)
+console.log(obj["Student"])
+
+
+function print(a,b)
+{
+    //console.log(a+b)
+    console.log(a-b)
+}
+print(2,"3")
+//+ -> right ka type.  in python
+//- -> left ka type
+
+//+ ->number, string consider
+//- ->preference number high, string low
+print(2,"2")
+print("2",2)
+
+
+const fn=()=>{
+    console.log
+}
