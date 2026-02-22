164811e48f66bf54d995a99661d7b80fa8bcdf0e
Anurag Nanda <nandaanurag13@gmail.com>
2026-02-22 15:54:44 +0530

    Sunday class Notes

--- Anurag-Nanda/Javascript/index.js
+++ Anurag-Nanda/Javascript/index.js
@@ -84,12 +84,67 @@ let name1;
 // it uses both global and blocked scope
 // const x=10;
 // x=11;  it is not possible 
-{
-   let x=10
-  console.log(x)
-  {
-    console.log(x)
-    let x=11
-  }
-  console.log(x)
+// {
+//    let x=10
+//   console.log(x)
+//   {
+//     // as here both local variable x and global var x both are present 
+//     console.log(x)
+//     // let x=11preference is first given to global scope
+//   }
+//   console.log(x)
+// }cd
+
+// Object and Arrays 
+// let arr =[1,2,3,4,5]
+// let obj={
+//     "Students":"Anurag",
+//     "Mentor":"Pratham"
+// }
+// console.log(arr)
+// console.log(obj)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(obj.Students)
+// console.log(obj["Students"])
+// console.log(obj["Mentor"])
+
+
+// function print(a,b){
+//     console.log(a-b)
+// }
+// console.log(2,"3")
+
+//+ number, string is considered
+// - preference number high, string low
+
+// function print(a,b){
+//     console.log(a+b)
+// }
+// console.log(2,"3")
+
+// const fn = (){
+//     console.log("Normal")
+// }
+
+// // const
+// function print(a,b){
+//     console.log(a+b)
+// }
+// const fun=() => {
+//     console.log("Arrow")
+// }
+
+// // High order functions
+// function ho(a,b,cb){
+//     console.log(a,b,cb)
+// }
+
+// console.log("Start")
+// set Timeout(finally,time in ms)
+// console.log("Start")
+// const data= 
+function name(){
+    console.log(arguments[0])
 }
+name(1,"Hello",3)
